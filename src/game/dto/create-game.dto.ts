95958774a2adfbfb272312e4b8eac829746a4274
Monica Dias <msumiedias@gmail.com>
2022-05-26 09:42:25 -0300

    feat(game): create dto 'CreateGameDto'

---
+++ src/game/dto/create-game.dto.ts
@@ -0,0 +1,9 @@
+export class CreateGameDto {
+  title: string;
+  converImageUrl: string;
+  description: string;
+  year: number;
+  imbdScore: number;
+  trailerYouTubeUrl: string;
+  gameplayYouTubeUrl: string;
+}
