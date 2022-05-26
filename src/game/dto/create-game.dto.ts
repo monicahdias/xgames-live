fa16ff1fc88935c60de6dc6d171cd96ba9b9d934
Monica Dias <msumiedias@gmail.com>
2022-05-26 10:17:54 -0300

    docs: implçement swagger

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -1,9 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber, IsPositive } from "class-validator";
 
 export class CreateGameDto {
   @IsNumber()
   @IsPositive()
+  @ApiProperty({
+    description: "The name of the game",
+    example: "The Legend of Zelda: Breath of the Wild",
+  })
   title: string;
+
+
   converImageUrl: string;
   description: string;
   year: number;
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('XGames Live')
+    .setDescription('Games Platform')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('game')
+    .addTag('user')
+    .addTag('profile')
+    .addTag('gender')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3333);
 }
