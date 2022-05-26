2bc2a5ee0c146ec5ddcd495c39a497f59a1cdeb6
Monica Dias <msumiedias@gmail.com>
2022-05-26 09:49:13 -0300

    feat(game): implement create game endpoint

---
+++ src/game/entities/game.entity.ts
@@ -0,0 +1,10 @@
+export class Game {
+  id: string;
+  title: string;
+  converImageUrl: string;
+  description: string;
+  year: number;
+  imbdScore: number;
+  trailerYouTubeUrl: string;
+  gameplayYouTubeUrl: string;
+}
