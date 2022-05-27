9ebb77132a9d3a15c4fa92e3df02c99a11006453
Monica Dias <msumiedias@gmail.com>
2022-05-27 10:58:50 -0300

    feat(game): implement update

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { CreateGameDto } from "./dto/create-game.dto";
+import { UpdateGameDto } from "./dto/update-game.dto";
 import { Game } from "./entities/game.entity";
 
 @Injectable()
 export class GameService {
+
   constructor(private readonly prisma: PrismaService) {}
 
   findAll(): Promise<Game[]> {
@@ -19,6 +21,14 @@ export class GameService {
     const data: Game = { ...dto}
 
     return this.prisma.game.create({data});
+  }
 
+  update(id: string, dto: UpdateGameDto): Promise<Game> {
+    const data: Partial<Game> = { ...dto}
+
+    return this.prisma.game.update({
+      where: { id },
+      data,
+    });
   }
 }
