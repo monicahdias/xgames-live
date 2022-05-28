3d09cfe99822441ed5228b119d66a912a7b82b58
Monica Dias <msumiedias@gmail.com>
2022-05-27 21:21:48 -0300

    feat(user): implement dto validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,7 +1,40 @@
+import { ApiProperty } from "@nestjs/swagger"
+import { IsBoolean, IsString } from "class-validator"
+
 export class CreateUserDto {
+
+  @IsString()
+  @ApiProperty({
+    description: "The user's name",
+    example: "Judite Maria",
+  })
   name: string
+
+  @IsString()
+  @ApiProperty({
+    description: "The user's email",
+    example: "juditemaria@gmail.com",
+  })
   email: string
+
+  @IsString()
+  @ApiProperty({
+    description: "The user's password",
+    example: "123456@Abcdef",
+  })
   password: string
+
+  @IsString()
+  @ApiProperty({
+    description: "The user's cpf",
+    example: "12345678901",
+  })
   cpf: string
+
+  @IsBoolean()
+  @ApiProperty({
+    description: "The user's isAdmin",
+    example: true,
+  })
   isAdmin: boolean
 }
