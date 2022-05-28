073b18f5ac78bc0bbee0defcfa8aaffd597dd98f
Monica Dias <msumiedias@gmail.com>
2022-05-27 22:18:19 -0300

    feat(user): hide 'password'

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,15 +7,30 @@ import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UserService {
+  private userSelect = {
+      id: true,
+        name: true,
+        email: true,
+        password: false,
+        cpf: true,
+        isAdmin: true,
+        createdAt: true,
+        updatedAt: true,
+  }
 
   constructor(private readonly prisma: PrismaService) {}
 
   findAll(): Promise<User[]> {
-    return this.prisma.user.findMany();
+    return this.prisma.user.findMany({
+      select: this.userSelect,
+    });
   }
 
   async findById(id: string): Promise<User> {
-    const record = await this.prisma.user.findUnique({ where: { id } });
+    const record = await this.prisma.user.findUnique({
+      where: { id },
+      select: this.userSelect,
+    });
 
     if(!record) {
       throw new NotFoundException(`User with id ${id} not found`);
@@ -40,7 +55,10 @@ export class UserService {
       password: await bcrypt.hash(dto.password, 10)
     };
 
-    return this.prisma.user.create({data}).catch(this.handleError);
+    return this.prisma.user.create({
+      data,
+      select: this.userSelect,
+    }).catch(this.handleError);
   }
 
   async update(id: string, dto: UpdateUserDto): Promise<User> {
@@ -63,6 +81,7 @@ export class UserService {
     return this.prisma.user.update({
       where: { id },
       data,
+      select: this.userSelect,
     })
     .catch(this.handleError);
   }
