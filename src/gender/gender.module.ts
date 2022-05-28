1110596a102b67606898952bc8ecbbca0980b9e7
Monica Dias <msumiedias@gmail.com>
2022-05-27 21:03:39 -0300

    feat(gender): implement dto & service

--- src/gender/gender.module.ts
+++ src/gender/gender.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GenderService } from './gender.service';
 import { GenderController } from './gender.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [GenderController],
   providers: [GenderService]
 })
--- src/gender/gender.service.ts
+++ src/gender/gender.service.ts
@@ -1,26 +1,59 @@
-import { Injectable } from '@nestjs/common';
-import { CreateGenderDto } from './dto/create-gender.dto';
-import { UpdateGenderDto } from './dto/update-gender.dto';
+import { Injectable, NotFoundException, UnprocessableEntityException } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateGenderDto } from "./dto/create-gender.dto";
+import { UpdateGenderDto } from "./dto/update-gender.dto";
+import { Gender } from "./entities/gender.entity";
 
 @Injectable()
 export class GenderService {
-  create(createGenderDto: CreateGenderDto) {
-    return 'This action adds a new gender';
+
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll(): Promise<Gender[]> {
+    return this.prisma.gender.findMany();
   }
 
-  findAll() {
-    return `This action returns all gender`;
+  async findById(id: string): Promise<Gender> {
+    const record = await this.prisma.gender.findUnique({ where: { id } });
+
+    if(!record) {
+      throw new NotFoundException(`Gender with id ${id} not found`);
+    }
+
+    return record;
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} gender`;
+  async findOne(id: string): Promise<Gender> {
+    return this.findById(id);
   }
 
-  update(id: string, updateGenderDto: UpdateGenderDto) {
-    return `This action updates a #${id} gender`;
+  create(dto: CreateGenderDto): Promise<Gender> {
+    const data: Gender = { ...dto}
+
+    return this.prisma.gender.create({data}).catch(this.handleError);
   }
 
-  delete(id: string) {
-    return `This action removes a #${id} gender`;
+  async update(id: string, dto: UpdateGenderDto): Promise<Gender> {
+    await this.findById(id);
+
+    const data: Partial<Gender> = { ...dto}
+
+    return this.prisma.gender.update({
+      where: { id },
+      data,
+    })
+    .catch(this.handleError);
+  }
+
+  async delete(id: string) {
+    await this.findById(id);
+
+    await this.prisma.gender.delete({ where: { id } });
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLine = errorLines[errorLines.length - 1];
+    throw new UnprocessableEntityException(lastErrorLine || 'An error occurred while processing your request');
   }
 }
