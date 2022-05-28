ebcfb560f00ae91998cae29f7c979f8b38c84ca4
Monica Dias <msumiedias@gmail.com>
2022-05-27 21:21:23 -0300

    feat(game): implement dto validation

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -1,20 +1,23 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNumber, IsPositive } from "class-validator";
+import { IsNumber, IsPositive, IsString, IsUrl } from "class-validator";
 
 export class CreateGameDto {
 
+  @IsString()
   @ApiProperty({
     description: "The game's title",
     example: "Valheim",
   })
   title: string;
 
+  @IsString()
   @ApiProperty({
     description: "The game's cover image url",
     example: "https://cdn.akamai.steamstatic.com/steamcommunity/public/images/clans/33695701/fa6ca8ee4d8f7a6cfd31a952b9b977eb370eee63.png",
   })
   coverimageurl: string;
 
+  @IsString()
   @ApiProperty({
     description: "The game's description",
     example: "Valheim is an open-world survival game played from a third-person perspective. As fallen Vikings, players must craft tools, build shelters and fight enemies to survive. The game uses distinctive low-resolution stylized 3D graphics, with a combat system inspired by action games. Co-operative gameplay with up to ten people and optional PvP gameplay is also supported.",
@@ -29,20 +32,24 @@ export class CreateGameDto {
   })
   year: number;
 
-  @IsNumber()
+  @IsNumber({
+    maxDecimalPlaces: 1,
+  })
   @IsPositive()
   @ApiProperty({
     description: "The game's IMDB Score",
-    example: 5,
+    example: 5.0,
   })
   imdbscore: number;
 
+  @IsUrl()
   @ApiProperty({
     description: "YouTube's trailer URL",
     example: "https://youtu.be/oNx-9YsOpBo",
   })
   traileryoutubeurl: string;
 
+  @IsUrl()
   @ApiProperty({
     description: "YouTube's gameplay URL",
     example: "https://youtu.be/oNx-9YsOpBo",
