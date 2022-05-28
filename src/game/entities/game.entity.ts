657fd95d5410d260157a7e1b2fe45e896e5ebebe
Monica Dias <msumiedias@gmail.com>
2022-05-28 19:35:59 -0300

    chore(game): add semicolon in entity

--- src/game/entities/game.entity.ts
+++ src/game/entities/game.entity.ts
@@ -1,12 +1,12 @@
 export class Game {
-  id?: string;
-  title: string;
-  coverimageurl: string;
-  description: string;
-  year: number;
-  imdbscore: number;
-  traileryoutubeurl: string;
-  gameplayyoutubeurl: string;
-  createdAt?: Date;
-  updatedAt?: Date;
+  id?: string
+  title: string
+  coverimageurl: string
+  description: string
+  year: number
+  imdbscore: number
+  traileryoutubeurl: string
+  gameplayyoutubeurl: string
+  createdAt?: Date
+  updatedAt?: Date
 }
