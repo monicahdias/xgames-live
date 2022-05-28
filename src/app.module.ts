706a7957bd0c1334e7f7c3697239dfe8a08aa6ae
Monica Dias <msumiedias@gmail.com>
2022-05-28 19:35:03 -0300

    change 'gender' to 'genre'

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { GenderModule } from './gender/gender.module';
 import { UserModule } from './user/user.module';
 import { ProfileModule } from './profile/profile.module';
+import { GenreModule } from './genre/genre.module';
 
 @Module({
-  imports: [GameModule, PrismaModule, GenderModule, UserModule, ProfileModule],
+  imports: [GameModule, PrismaModule, GenreModule, UserModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
