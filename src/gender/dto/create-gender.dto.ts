07ce3f587ae7fc5c4015b6e6b21ec02e7f7fb51e
Monica Dias <msumiedias@gmail.com>
2022-05-27 21:21:33 -0300

    feat(gender): implement dto validation

--- src/gender/dto/create-gender.dto.ts
+++ src/gender/dto/create-gender.dto.ts
@@ -1,3 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+
 export class CreateGenderDto {
+  @IsString()
+  @ApiProperty({
+    description: "The gender's name",
+    example: "Adventure",
+  })
   name: string;
 }
